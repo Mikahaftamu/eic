1eff1fd96d67e5ec165282fb2b21a53347719470
mchealay <haftemchealay@gmail.com>
2025-04-30 23:54:05 +0300

    the timeStamp of coveragePlan is changed

--- src/database/migrations/1711800000000-CreateCoveragePlanTable.ts
+++ src/database/migrations/1711800000000-CreateCoveragePlanTable.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CreateCoveragePlanTable1711800000000 implements MigrationInterface {
-    name = 'CreateCoveragePlanTable1711800000000'
+export class CreateCoveragePlanTable1711800000002 implements MigrationInterface {
+    name = 'CreateCoveragePlanTable1711800000002'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
